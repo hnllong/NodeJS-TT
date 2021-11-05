f250412446076700b7cf1711f0de8809d8883966
hieulv <hieu.lv@zinza.com.vn>
2021-11-05 15:34:24 +0700

    hieulv - api/user documentation

--- src/openApiDocumentation.js
+++ src/openApiDocumentation.js
@@ -10,7 +10,7 @@ export const openApiDocumentation = {
     contact: {
       name: "LVH",
       email: "hieu.lv@zinza.com.vn",
-      url: "http://localhost:5000/api-docs",
+      url: "http://localhost:5000/api/docs/v1",
     },
     license: {
       name: "Apache 2.0",
@@ -33,7 +33,7 @@ export const openApiDocumentation = {
   ],
   security: [
     {
-      ApiKeyAuth: [],
+      access_token: [],
     },
   ],
   tags: [
@@ -54,11 +54,13 @@ export const openApiDocumentation = {
     },
   ],
   paths: {
+    // -----user routes-----
     "/user/login": {
       post: {
         tags: ["users"],
+        summary: "Api login",
         description:
-          "If you are an admin, please use the existing account of the system, the rest use the account created by the admin and log in to the system",
+          "If you are admin, please use the existing account of the system, the rest use the account created by the admin and log in to the system",
         operationId: "Login",
         requestBody: {
           content: {
@@ -80,7 +82,8 @@ export const openApiDocumentation = {
         },
         responses: {
           200: {
-            description: "Success!",
+            description:
+              "Request was successful.[success: true || success: false]",
             content: {
               "application/json": {
                 schema: {
@@ -106,10 +109,485 @@ export const openApiDocumentation = {
         },
       },
     },
+
+    "/user/create": {
+      post: {
+        tags: ["users"],
+        summary: "Api create new user",
+        description:
+          "If you are admin, create a new user. Otherwise you do not have permission to execute this api",
+        operationId: "Create",
+        requestBody: {
+          content: {
+            "application/json": {
+              schema: {
+                type: "object",
+                properties: {
+                  email: {
+                    $ref: "#/components/schemas/email",
+                  },
+                  password: {
+                    $ref: "#/components/schemas/password",
+                  },
+                  fullName: {
+                    $ref: "#/components/schemas/fullName",
+                  },
+                  role: {
+                    $ref: "#/components/schemas/role",
+                  },
+                  gender: {
+                    $ref: "#/components/schemas/gender",
+                  },
+                },
+                required: ["email", "password", "fullName", "role", "gender"],
+              },
+            },
+          },
+        },
+        responses: {
+          200: {
+            description:
+              "Request was successful.[success: true || success: false]",
+            content: {
+              "application/json": {
+                schema: {
+                  type: "object",
+                  properties: {
+                    success: {
+                      $ref: "#/components/schemas/success",
+                    },
+                    message: {
+                      $ref: "#/components/schemas/message",
+                    },
+                    data: {
+                      $ref: "#/components/schemas/User",
+                    },
+                  },
+                  required: ["success", "message", "data"],
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+
+    "/user/reset-password": {
+      put: {
+        tags: ["users"],
+        summary: "Api reset one or more user",
+        description: "If you are admin, you can reset one or more passwords",
+        operationId: "ResetPassword",
+        requestBody: {
+          content: {
+            "application/json": {
+              schema: {
+                type: "object",
+                properties: {
+                  arrayId: {
+                    type: "array",
+                    description: "Array userId want to delete",
+                    example: ["userId1", "userId2"],
+                  },
+                },
+                required: ["arrayId"],
+              },
+            },
+          },
+        },
+        responses: {
+          200: {
+            description:
+              "Request was successful.[success: true || success: false]",
+            content: {
+              "application/json": {
+                schema: {
+                  $ref: "#/components/schemas/Response",
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+
+    "/user/info": {
+      get: {
+        tags: ["users"],
+        summary: "Api get information of user who is logged in",
+        description: "Get information of logged in user",
+        operationId: "GetInfo",
+        responses: {
+          200: {
+            description:
+              "Request was successful.[success: true || success: false]",
+            content: {
+              "application/json": {
+                schema: {
+                  type: "object",
+                  properties: {
+                    success: {
+                      $ref: "#/components/schemas/success",
+                    },
+                    message: {
+                      $ref: "#/components/schemas/message",
+                    },
+                    data: {
+                      $ref: "#/components/schemas/User",
+                    },
+                  },
+                  required: ["success", "message", "data"],
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+
+    "/user/list": {
+      post: {
+        tags: ["users"],
+        summary: "Api get all users",
+        description: "Returns list of users",
+        operationId: "GetList",
+        requestBody: {
+          content: {
+            "application/json": {
+              schema: {
+                type: "object",
+                properties: {
+                  currentPage: {
+                    $ref: "#/components/schemas/currentPage",
+                  },
+                  pageSize: {
+                    $ref: "#/components/schemas/pageSize",
+                  },
+                  context: {
+                    $ref: "#/components/schemas/context",
+                  },
+                },
+              },
+            },
+          },
+        },
+        responses: {
+          200: {
+            description:
+              "Request was successful.[success: true || success: false]",
+            content: {
+              "application/json": {
+                schema: {
+                  type: "object",
+                  properties: {
+                    success: {
+                      $ref: "#/components/schemas/success",
+                    },
+                    message: {
+                      $ref: "#/components/schemas/message",
+                    },
+                    data: {
+                      total: {
+                        type: "integer",
+                        description: "total number of users",
+                        example: 100,
+                      },
+                      totalPage: {
+                        type: "integer",
+                        description: "total number of pages",
+                        example: 10,
+                      },
+                      $ref: "#/components/schemas/Users",
+                    },
+                  },
+                  required: ["success", "message", "data"],
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+
+    "/user/change-password": {
+      put: {
+        tags: ["users"],
+        summary: "Api change password",
+        description: "Change password",
+        operationId: "ChangePassword",
+        requestBody: {
+          content: {
+            "application/json": {
+              schema: {
+                type: "object",
+                properties: {
+                  oldPassword: {
+                    type: "string",
+                    description: "old password",
+                    example: "123123",
+                  },
+                  newPassword: {
+                    type: "string",
+                    description: "new password",
+                    example: "123456",
+                  },
+                },
+                required: ["ollPassword", "newPassword"],
+              },
+            },
+          },
+        },
+        responses: {
+          200: {
+            description:
+              "Request was successful.[success: true || success: false]",
+            content: {
+              "application/json": {
+                schema: {
+                  $ref: "#/components/schemas/Response",
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+
+    "/user/delete": {
+      delete: {
+        tags: ["users"],
+        summary: "Api delete one or more users",
+        description: "If you are admin, you can delete one or more users",
+        operationId: "DeleteUser",
+        requestBody: {
+          content: {
+            "application/json": {
+              schema: {
+                type: "object",
+                properties: {
+                  arrayId: {
+                    type: "array",
+                    description: "Array userId want to delete",
+                    example: ["userId1", "userId2"],
+                  },
+                },
+                required: ["arrayId"],
+              },
+            },
+          },
+        },
+        responses: {
+          200: {
+            description:
+              "Request was successful.[success: true || success: false]",
+            content: {
+              "application/json": {
+                schema: {
+                  type: "object",
+                  properties: {
+                    success: {
+                      $ref: "#/components/schemas/success",
+                    },
+                    message: {
+                      $ref: "#/components/schemas/message",
+                    },
+                    data: {
+                      type: "array",
+                      description: "Deleted userId array",
+                      example: ["userId1", "userId2"],
+                    },
+                  },
+                  required: ["success", "message", "data"],
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+
+    "/user/update/{id}": {
+      put: {
+        tags: ["users"],
+        summary: "Api update user",
+        description:
+          "If user update my profile then can update fullName, dateOfBirth, gender, address, role, joinCompanyAt, phone, avatar. If admin then There will be the following cases: update role for manager role: 2 -> role: 1 true when department of manager: []. If manager don't update department of manager. Update staff -> manager => department: []. If staff can update department of staff",
+        operationId: "UpdateUser",
+        parameters: [
+          {
+            name: "id",
+            in: "path",
+            schema: {
+              type: "string",
+            },
+            required: true,
+          },
+        ],
+        requestBody: {
+          content: {
+            "application/json": {
+              schema: {
+                type: "object",
+                properties: {
+                  fullName: {
+                    $ref: "#/components/schemas/fullName",
+                  },
+                  dateOfBirth: {
+                    $ref: "#/components/schemas/dateOfBirth",
+                  },
+                  gender: {
+                    $ref: "#/components/schemas/gender",
+                  },
+                  address: {
+                    $ref: "#/components/schemas/address",
+                  },
+                  role: {
+                    $ref: "#/components/schemas/role",
+                  },
+                  department: {
+                    $ref: "#/components/schemas/department",
+                  },
+                  joinCompanyAt: {
+                    $ref: "#/components/schemas/joinCompanyAt",
+                  },
+                  phone: {
+                    $ref: "#/components/schemas/phone",
+                  },
+                  avatar: {
+                    $ref: "#/components/schemas/avatar",
+                  },
+                },
+              },
+            },
+          },
+        },
+        responses: {
+          200: {
+            description:
+              "Request was successful.[success: true || success: false]",
+            schema: {
+              type: "object",
+              properties: {
+                success: {
+                  $ref: "#/components/schemas/success",
+                },
+                message: {
+                  $ref: "#/components/schemas/message",
+                },
+                data: {
+                  $ref: "#/components/schemas/User",
+                },
+              },
+              required: ["success", "message", "data"],
+            },
+          },
+        },
+      },
+    },
+
+    "/user/view/{id}": {
+      get: {
+        tags: ["users"],
+        summary: "Api view information user",
+        description:
+          "Admin can view info of user. Manager can view info of user in department",
+        operationId: "ViewUser",
+        parameters: [
+          {
+            name: "id",
+            in: "path",
+            schema: {
+              type: "string",
+            },
+            required: true,
+          },
+        ],
+        responses: {
+          200: {
+            description:
+              "Request was successful.[success: true || success: false]",
+            schema: {
+              type: "object",
+              properties: {
+                success: {
+                  $ref: "#/components/schemas/success",
+                },
+                message: {
+                  $ref: "#/components/schemas/message",
+                },
+                data: {
+                  $ref: "#/components/schemas/User",
+                },
+              },
+              required: ["success", "message", "data"],
+            },
+          },
+        },
+      },
+    },
+
+    // -----department routes-----
+    "/department/list": {},
+
+    "/department/create": {},
+
+    "/department/update/{id}": {},
+
+    "/department/delete/{id}": {},
+
+    "/department/read/{id}": {},
+
+    "/department/list-user": {},
+
+    "/department/export-staff-list": {},
+
+    // -----request routes-----
+    "/request/create": {},
+
+    "/request/accept/{id}": {},
+
+    "/request/refuse/{id}": {},
+
+    "/request/user-list": {},
+
+    "/request/member-list": {},
+
+    // -----timeSheet routes-----
+    "/timesheet/check-in": {},
+
+    "/timesheet/check-out/{id}": {},
+
+    "/timesheet/list": {},
+
+    "/timesheet/work-date/{id}": {},
+
+    // -----file routes-----
+    "/file/upload": {},
+
+    "/file/{key}": {},
+
+    "/file/{key}": {},
   },
   components: {
     schemas: {
-      // response
+      // -----pagination && search-----
+      currentPage: {
+        type: "integer",
+        description: "Current page",
+        example: 1,
+      },
+      pageSize: {
+        type: "integer",
+        description: "limit of a page",
+        example: 5,
+      },
+      context: {
+        type: "string",
+        description: "the keyword to search",
+        example: "name",
+      },
+
+      // -----response-----
       success: {
         type: "boolean",
         description: "The status of the request",
@@ -120,7 +598,8 @@ export const openApiDocumentation = {
         description: "Return request's message",
         example: "Internal server error",
       },
-      // user
+
+      // -----user properties-----
       email: {
         type: "string",
         description: "Email address",
@@ -187,6 +666,8 @@ export const openApiDocumentation = {
         description: "Phone number",
         example: "0123456789",
       },
+
+      // -----UserModel-----
       User: {
         type: "object",
         properties: {
@@ -228,6 +709,8 @@ export const openApiDocumentation = {
           },
         },
       },
+
+      // -----list user-----
       Users: {
         type: "object",
         properties: {
@@ -239,6 +722,7 @@ export const openApiDocumentation = {
           },
         },
       },
+      // -----response-----
       Response: {
         type: "object",
         properties: {
@@ -251,11 +735,12 @@ export const openApiDocumentation = {
         },
       },
     },
+
     securitySchemes: {
-      accessToken: {
+      access_token: {
         type: "apiKey",
         in: "header",
-        name: "accessToken",
+        name: "access_token",
       },
     },
   },
