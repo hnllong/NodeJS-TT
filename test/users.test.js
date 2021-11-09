3e31c27af28c510b88c074bb7011392c7c24dc68
hieulv <hieu.lv@zinza.com.vn>
2021-11-09 15:25:18 +0700

    hieulv - config testing, e2e user/login

---
+++ test/users.test.js
@@ -0,0 +1,19 @@
+import request from "supertest";
+import app from "../src/index.js";
+
+describe("POST /api/v1/user/login", function () {
+  it("Log in to the system", function (done) {
+    request(app)
+      .post("/api/v1/user/login")
+      .send({ email: "hieu.lv@zinza.com.vn", password: "123123" })
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(200, done())
+      .then((response) => {
+        assert(response.body.data !== undefined);
+      })
+      .catch((err) => {
+        assert(err === undefined);
+      });
+  });
+});
