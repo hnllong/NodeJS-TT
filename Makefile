319972bf126ec65e60f254d5dde5f1de7398aac9
devops <yhieu.lv@zinza.com.vn>
2021-11-30 14:39:47 +0700

    hieulv - mongo docker

--- Makefile
+++ Makefile
@@ -1,8 +1,11 @@
-up:        
+up-dev:        
         docker-compose up -d --build
 
 down: 
         docker-compose down
+
+up-stag:
+        docker-compose -f docker-compose.stag.yml up -d
 
 up-prod:
         docker-compose -f docker-compose.prod.yml up -d
--- src/config/mongoose.js
+++ src/config/mongoose.js
@@ -1,28 +1,14 @@
 import mongoose from "mongoose";
 import { environment } from "../config/index.js";
 
-const connectionString = "mongodb://mongo:27017/cinema";
-
-export const connectionMongodb = () => {
-  mongoose
-    .connect(
-      `mongodb+srv://${environment.mongo_db.user}:${environment.mongo_db.password}@cluster0.tdbxe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
-      { useNewUrlParser: true, useUnifiedTopology: true }
-    )
-    .then(() => {
-      console.log("Connected to DB");
-    })
-    .catch((err) => {
-      console.log("err", err);
-    });
-};
+const connectionString = "mongodb://mongo:27017/training-db";
 
 // export const connectionMongodb = () => {
 //   mongoose
-//     .connect(connectionString, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     })
+//     .connect(
+//       `mongodb+srv://${environment.mongo_db.user}:${environment.mongo_db.password}@cluster0.tdbxe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+//       { useNewUrlParser: true, useUnifiedTopology: true }
+//     )
 //     .then(() => {
 //       console.log("Connected to DB");
 //     })
@@ -30,3 +16,17 @@ export const connectionMongodb = () => {
 //       console.log("err", err);
 //     });
 // };
+
+export const connectionMongodb = () => {
+  mongoose
+    .connect(connectionString, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("Connected to MONGO-DB-IMAGE");
+    })
+    .catch((err) => {
+      console.log("err", err);
+    });
+};
